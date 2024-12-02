1666c0ff1a1616c351f81c2969591181664ebe25
Sumit Vekariya <sarkazein7@gmail.com>
2024-12-03 01:45:12 +0530

    structuredClone error fix

--- src/shim.ts
+++ src/shim.ts
@@ -4,5 +4,12 @@ import 'react-native-console-time-polyfill';
 import 'react-native-get-random-values';
 import 'text-encoding-polyfill';
 
+// Attach the polyfill as a Global function
+import structuredClone from "@ungap/structured-clone";
+if (!("structuredClone" in globalThis)) {
+  globalThis.structuredClone = structuredClone;
+}
 // @ts-ignore
-global.Worker = {};
+global.Worker = {
+  'structuredClone': structuredClone
+};
